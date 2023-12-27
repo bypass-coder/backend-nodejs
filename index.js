df2d56b24d27cb54081619c1f92395d5ea95ddab
bypass-coder <chiragdraul@gmail.com>
2023-12-27 10:52:09 +0530

    modified:   index.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+dotenv.config()
 const PORT = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.PASS}@cluster0.krk0gyq.mongodb.net/?retryWrites=true&w=majority`
 
-app.use();
+app.use(express.json()); // middleware...
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/',(req,res)=>{
+    res.json({
+        status: 200,
+        message:"your server is up and running",
+        uri: uri
+    })
+})
+
+app.get('/home',(req,res)=>{
+    res.json("this is a home router...")
+})
 
 app.listen(PORT,()=>{
     console.log(`listening on port number -> ${PORT}`);
